4c7cbb1045ac6885cd083f1161a35edbe1c9af76
jjmedinamartin <jjmm89@gmail.com>
2020-06-07 11:28:15 +0200

    refactor

--- api/Controllers/projectController.js
+++ api/Controllers/projectController.js
@@ -4,8 +4,6 @@ User = require('../Models/userModel');
 exports.list = function(req, res) {
     Project.find().then(projects => {
         res.json({
-            status: "OK",
-            code: "200",
             data: projects
         });
     }).catch(err => {
@@ -16,8 +14,6 @@ exports.list = function(req, res) {
 exports.get = function (req, res) {
     Project.find({system_id: req.params.project_id}).then(project => {
         res.json({
-            status: "OK",
-            code: "200",
             data: project
         });
     }).catch(err => {
@@ -29,8 +25,6 @@ exports.getByUser = function(req, res) {
     User.findOne({system_id: req.params.user_id}, '_id').then(user_object => {
         Project.find({owner: user_object}).then(projects => {
             res.json({
-                status: "OK",
-                code: "200",
                 data: projects
             });
         }).catch(err => {

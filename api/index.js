7ce9c2a86f77465a552217bbfe50a958bc7f0e65
jjmedinamartin <jjmm89@gmail.com>
2020-06-07 11:32:39 +0200

    refactor

--- api/index.js
+++ api/index.js
@@ -4,22 +4,22 @@ let apiRoutes = require("./api-routes")
 let bodyParser = require ('body-parser');
 let mongoose = require ('mongoose');
 var port = process.env.PORT || 8000;
+var conectionUrl = 'mongodb://localhost:27017/ageectDB';
 
 app.use(bodyParser.urlencoded({
     extended: true
  }));
  app.use(bodyParser.json());
- mongoose.connect('mongodb://localhost:27017/ageectDB', { useNewUrlParser: true});
+ mongoose.connect(conectionUrl, { useNewUrlParser: true});
  var db = mongoose.connection;
 
  // Added check for DB connection
 if(!db)
-console.log("Error connecting db")
+console.log("Error connecting ageectDB")
 else
-console.log("Db connected successfully")
+console.log("AgeectDB connected successfully")
 
 app.use('/api', apiRoutes)
-app.get('/', (req, res) => res.send('Hello World with Express'));
 app.listen(port, function () {
     console.log("Running RestHub on port " + port);
 });
